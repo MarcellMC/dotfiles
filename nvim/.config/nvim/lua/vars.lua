91af4dcc8d656d5407ea68843af6038492604a82
Marc Moiseev <marcell89@gmail.com>
2022-08-05 02:43:51 +0300

    Disable unused language providers

--- nvim/.config/nvim/lua/vars.lua
+++ nvim/.config/nvim/lua/vars.lua
@@ -7,3 +7,9 @@ g.background = "dark"
 -- Update the packpath
 local packer_path = vim.fn.stdpath('config') .. '/site'
 vim.o.packpath = vim.o.packpath .. ',' .. packer_path
+
+-- [[ Language Providers ]]
+g.loaded_python3_provider = 0
+g.loaded_node_provider = 0
+g.loaded_perl_provider = 0
+
