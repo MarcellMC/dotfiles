7cfe228680146642c33492e461859a8f0d7f9aee
Marc Moiseev <marcell89@gmail.com>
2022-08-24 17:10:27 +0300

    Set up LSP keymaps

--- nvim/.config/nvim/lua/keys.lua
+++ nvim/.config/nvim/lua/keys.lua
@@ -9,7 +9,7 @@ local nnoremap = require("keyset").nnoremap
 map('n', '<leader>ve', [[:edit $MYVIMRC<CR>]], {})
 map('n', '<leader>vr', [[:source $MYVIMRC<CR>]], {})
 map('n', '<leader>vi', [[:source $MYVIMRC<CR> :PackerInstall<CR>]], {})
-nnoremap('<leader>vs', '<CMD>PackerSync<CR>')
+nnoremap('<leader>vs', '<cmd>luafile %<CR>')
 
 -- Resize windows with Ctrl-Arrows
 map('n', '<C-Up>', ':resize +2<CR>', {})
@@ -22,13 +22,46 @@ map('n', '<leader>l', [[:IndentLinesToggle<CR>]], {})
 -- map('n', '<leader>t', [[:TagbarToggle<CR>]], {})
 
 -- LSP
-nnoremap('<leader>h', vim.lsp.buf.hover, {buffer=0})
-nnoremap('<leader>gd', vim.lsp.buf.definition, {buffer=0})
-nnoremap('<leader>ne', vim.diagnostic.goto_next, {buffer=0})
-nnoremap('<leader>pe', vim.diagnostic.goto_prev, {buffer=0})
-nnoremap('<leader>r', vim.lsp.buf.rename, {buffer=0})
-nnoremap('<leader>ca', vim.lsp.buf.code_action, {buffer=0})
-nnoremap('<leader>fe', '<CMD>Telescope diagnostics<CR>', {buffer=0})
+
+-- hover
+nnoremap('<F1>', '<cmd>Lspsaga hover_doc<CR>', { silent = true })
+nnoremap('K', vim.lsp.buf.hover, {buffer=0})
+
+-- definitions, declarations, implementations
+nnoremap('gd', vim.lsp.buf.definition, {buffer=0})
+nnoremap('gr', vim.lsp.buf.references, {buffer=0})
+nnoremap('<leader>cpd', '<cmd>Lspsaga preview_definition<CR>', { silent = true })
+
+-- diagnostics
+nnoremap('[D', vim.diagnostic.goto_next, {buffer=0})
+nnoremap(']D', vim.diagnostic.goto_prev, {buffer=0})
+nnoremap('[d', '<cmd>Lspsaga diagnostic_jump_next<CR>', { silent = true })
+nnoremap(']d', '<cmd>Lspsaga diagnostic_jump_prev<CR>', { silent = true })
+nnoremap('<leader>cld>', '<cmd>Lspsaga show_line_diagnostics<CR>', { silent = true })
+nnoremap('<leader>ccd>', '<cmd>Lspsaga show_cursor_diagnostics<CR>', { silent = true })
+
+-- rename
+nnoremap('<leader>cr', vim.lsp.buf.rename, {buffer=0})
+nnoremap('<F2>', '<cmd>Lspsaga rename<CR>', { silent = true })
+
+-- code actions
+-- nnoremap('<leader>ca', vim.lsp.buf.code_action, {buffer=0})
+nnoremap('<leader>ca', '<cmd>Lspsaga code_action<CR>')
+-- vnoremap('v', '<leader>ca', '<cmd><C-U>Lspsaga range_code_action<CR>', { silent = true })
+
+
+nnoremap('<leader>cf>', '<cmd>Lspsaga lsp_finder<CR>')
+nnoremap('<leader>co', '<cmd>LSoutlineToggle<CR>',{ silent = true })
+
+-- local action = require("lspsaga.action")
+-- -- scroll in hover doc or  definition preview window
+-- vim.keymap.set("n", "<C-f>", function()
+--     action.smart_scroll_with_saga(1)
+-- end, { silent = true })
+-- -- scroll in hover doc or  definition preview window
+-- vim.keymap.set("n", "<C-b>", function()
+--     action.smart_scroll_with_saga(-1)
+-- end, { silent = true })
 
 -- Telescope
 map('n', '<leader>ff', [[:Telescope find_files hidden=true<CR>]], {})
@@ -38,6 +71,7 @@ map('n', '<leader>fb', [[:Telescope buffers<CR>]], {})
 map('n', '<leader>fh', [[:Telescope find_help<CR>]], {})
 -- map('n', '<leader>fm', [[:Telescope harpoon marks<CR>]], {})
 map('n', '<leader>fw', [[:lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>]], {})
+nnoremap('<leader>fe', '<cmd>Telescope diagnostics<CR>', {buffer=0})
 
 -- Fugitive
 map('n', '<leader>gs', ':0Git<CR>', {})
