df36e1afd69eb308a69304134a13ed2f5c9310f4
Marc Moiseev <marcell89@gmail.com>
2022-08-24 15:35:43 +0300

    - Update plugins

--- nvim/.config/nvim/lua/plug.lua
+++ nvim/.config/nvim/lua/plug.lua
@@ -1,14 +1,18 @@
 -- [[ plug.lua ]]
 
 -- PACKER
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
+-- local fn = vim.fn
+-- local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+-- if fn.empty(fn.glob(install_path)) > 0 then
+--     packer_bootstrap = fn.system({'git',
+--     'clone', '--depth',
+--     '1',
+--     'https://github.com/wbthomason/packer.nvim',
+--     install_path})
+-- end
 
-return require('packer').startup(function()
-    use { 'wbthomason/packer.nvim' }
+return require('packer').startup(function(use)
+    use ( 'wbthomason/packer.nvim' )
 
     -- [[ TreeSitter ]]
     use {
@@ -35,42 +39,42 @@ return require('packer').startup(function()
     use {                                              -- filesystem navigation
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons'        -- filesystem icons
-}
+    }
 
--- [[ Telescope ]]
-use {
-    'nvim-telescope/telescope.nvim',                 -- fuzzy finder
-    requires = { {'nvim-lua/plenary.nvim'} }
-}
-use { 'ThePrimeagen/git-worktree.nvim' }
+    -- [[ Telescope ]]
+    use {
+        'nvim-telescope/telescope.nvim',                 -- fuzzy finder
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use { 'ThePrimeagen/git-worktree.nvim' }
 
--- [[ Theme ]]
---  use { 'mhinz/vim-startify' }                       -- start screen
-use { 'DanilaMihailov/beacon.nvim' }               -- cursor jump
-use {
-    'nvim-lualine/lualine.nvim',                     -- statusline
-    requires = {'kyazdani42/nvim-web-devicons',
-    opt = true}
-}
-use {
-    "catppuccin/nvim",                               -- catpuccin theme
-    as = "catppuccin"
-}
+    -- [[ Theme ]]
+    use { 'mhinz/vim-startify' }                       -- start screen
+    use { 'DanilaMihailov/beacon.nvim' }               -- cursor jump
+    use {
+        'nvim-lualine/lualine.nvim',                     -- statusline
+        requires = {'kyazdani42/nvim-web-devicons',
+        opt = true}
+    }
+    use {
+        "catppuccin/nvim",                               -- catpuccin theme
+        as = "catppuccin"
+    }
 
--- [[ Dev ]]
-use { 'majutsushi/tagbar' }                        -- code structure
-use { 'Yggdroot/indentLine' }                      -- see indentation
-use { 'tpope/vim-fugitive' }                       -- git integration
-use { 'junegunn/gv.vim' }                          -- commit history
-use { 'windwp/nvim-autopairs' }                    -- auto-insert pairing symbols
-use { 'tpope/vim-commentary' }                     -- shortcuts for commenting code
-use { 'tpope/vim-surround' }                       -- surround with ease
-use { 'tpope/vim-repeat' }                         -- enhance vim's '.' (repeat last action)
-use { 'tpope/vim-speeddating' }                    -- advanced dates
-use { 'tpope/vim-eunuch' }                         -- sugar for the UNIX shell commands
-use { 'godlygeek/tabular' }                        -- easy alignment
+    -- [[ Dev ]]
+    use { 'majutsushi/tagbar' }                        -- code structure
+    use { 'Yggdroot/indentLine' }                      -- see indentation
+    use { 'tpope/vim-fugitive' }                       -- git integration
+    use { 'junegunn/gv.vim' }                          -- commit history
+    use { 'windwp/nvim-autopairs' }                    -- auto-insert pairing symbols
+    use { 'tpope/vim-commentary' }                     -- shortcuts for commenting code
+    use { 'tpope/vim-surround' }                       -- surround with ease
+    use { 'tpope/vim-repeat' }                         -- enhance vim's '.' (repeat last action)
+    use { 'tpope/vim-speeddating' }                    -- advanced dates
+    use { 'tpope/vim-eunuch' }                         -- sugar for the UNIX shell commands
+    use { 'godlygeek/tabular' }                        -- easy alignment
 
-if packer_bootstrap then
-    require('packer').sync()
-end
+    -- if packer_bootstrap then
+    --     require('packer').sync()
+    -- end
 end)
