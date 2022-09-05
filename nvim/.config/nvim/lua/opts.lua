828e336a183eb86c1bf0c9360cfe4311964287f1
Marc Moiseev <marcell89@gmail.com>
2022-09-05 22:19:02 +0300

    Set key timeout length to 300 ms

--- nvim/.config/nvim/lua/opts.lua
+++ nvim/.config/nvim/lua/opts.lua
@@ -31,3 +31,6 @@ opt.tabstop = 4                  -- num:  Number of spaces tabs count for
 -- [[ Splits ]]
 opt.splitright = true            -- bool: Place new window to right of current one
 opt.splitbelow = true            -- bool: Place new window below the current one
+
+-- [[ Timeout ]]
+opt.timeoutlen = 300
