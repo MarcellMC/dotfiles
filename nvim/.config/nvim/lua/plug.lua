9d165789e4f2992d537851b7dc217e1992fa3b05
Marc Moiseev <marcell89@gmail.com>
2023-01-24 18:44:25 +0200

    Install GitSigns plugin

--- nvim/.config/nvim/lua/plug.lua
+++ nvim/.config/nvim/lua/plug.lua
@@ -80,6 +80,10 @@ return require('packer').startup(function(use)
     use { 'idanarye/vim-merginal' }                       -- git integration
     use { 'rbong/vim-flog' }                           -- git branch viewer
     use { 'ThePrimeagen/git-worktree.nvim' }           -- git worktree integration
+    use {
+        'lewis6991/gitsigns.nvim',
+        tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+    }
 
     -- [[ Dev ]]
     use { 'majutsushi/tagbar' }                        -- code structure
