3637e5499b0545b8a82baa7817140f3ce1bb9207
Marc Moiseev <marcell89@gmail.com>
2025-12-13 18:49:48 +0200

    Add lazygit config to dotfiles

---
+++ lazyvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  -- Add catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    },
+  },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
